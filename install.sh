d36f1f4017e84135926860db9db9aedade0f3767
David Conmy <david.conmy@sap.com>
2019-03-28 10:01:19 +0000

    Added kubectl installation steps

--- install.sh
+++ install.sh
@@ -14,7 +14,9 @@ if [[ "$OSTYPE" = 'msys' ]]; then
 	if [[ ! -f pacman.list ]]; then
 		echo "no external packages installed."
 	else
-		mkdir ./tmp
+        if [[ ! -d ./tmp ]]; then
+            mkdir ./tmp
+        fi
 		cd ./tmp
 
 		# Download and extract packages not usually packaged with git for windows
@@ -23,7 +25,6 @@ if [[ "$OSTYPE" = 'msys' ]]; then
 			curl $PACMAN_URL/$package -O
 			tar -xvJf $package usr/bin/
 			echo "Extracted $package to ./tmp directory"
-		echo $package
 		done < ../pacman.list
 
 		if [[ -d usr/bin ]]; then
@@ -39,23 +40,44 @@ if [[ "$OSTYPE" = 'msys' ]]; then
 		fi
         cd ~-
 	fi
+
+    # Kubectl variables
+    kube_binary=kubectl.exe
+    kube_arch=windows/amd64
 else
 	# OTHER OS SPECIFIC CONFIG GOES HERE
     echo ""
+
+    # Assuming Linux
+    kube_binary=kubectl
+    kube_arch=linux/amd64
 fi
 
+# Install vim_runtime for awesome vim
 if [[ ! -d ~/.vim_runtime ]]; then
     echo "Installing awesome vim"
 	git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
 	sh ~/.vimruntime/install_awesome_vimrc.sh
 fi
 
+# Install kubectl
+## TODO: Add check for existance of variables before executing this code?
+if [[ ! -f $DEV_BIN/$kube_binary ]]; then
+
+    echo "Downloading kubectl and placing it in $DEV_BIN"
+    kubelatest=`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`
+    curl -L https://storage.googleapis.com/kubernetes-release/release/$kubelatest/bin/$kube_arch/$kube_binary -o $DEV_BIN/$kube_binary
+else
+    echo "kubectl already installed"
+    # Include upgrade steps here for kubectl.
+fi
+
 # move the home files to the home directory of the user.
-echo "Moving home dotfiles to home directory"
+echo "Moving home dotfiles to the user's home directory"
 cp -burv ./home/* ~/
 if [[ $? -eq 0 ]]; then
-    echo "SUCCESS"
+    echo "SUCCESS: dotfiles moved to user's home directory"
 else
-    echo "FAILED"
+    echo "FAILED to move dotfiles to the user's home directory"
     exit 1;
 fi
